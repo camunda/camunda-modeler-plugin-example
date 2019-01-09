dbadf3b215249da7b84bdf2a6cc8823d9d01d55e
Nico Rehwaldt <git_nikku@nixis.de>
2019-01-09 14:40:19 +0100

    chore(example): rebuild client plug-in

--- client/client-bundle.js
+++ client/client-bundle.js
@@ -1,14 +1,131 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-'use strict';
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./client/client.js");
+/******/ })
+/************************************************************************/
+/******/ ({
 
-var assign = Object.assign;
+/***/ "./client/client.js":
+/*!**************************!*\
+  !*** ./client/client.js ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-function ClientPlugin(eventBus, canvas) {
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var camunda_modeler_plugin_helpers__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! camunda-modeler-plugin-helpers */ "./node_modules/camunda-modeler-plugin-helpers/index.js");
+/* harmony import */ var camunda_modeler_plugin_helpers__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(camunda_modeler_plugin_helpers__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./module */ "./client/module.js");
+
+
+
+
+Object(camunda_modeler_plugin_helpers__WEBPACK_IMPORTED_MODULE_0__["registerBpmnJSPlugin"])(_module__WEBPACK_IMPORTED_MODULE_1__["default"]);
+
+
+/***/ }),
+
+/***/ "./client/module.js":
+/*!**************************!*\
+  !*** ./client/module.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/**
+ * A bpmn-js extension service, providing the actual
+ * plug-in feature.
+ */
+function ExamplePluginService(eventBus, canvas) {
   var img = document.createElement('img');
   img.src = getPluginsDirectory() + '/camunda-modeler-plugin-example/logo.png';
   img.width = '50';
 
-  assign(img.style, {
+  Object.assign(img.style, {
     position: 'absolute',
     bottom: '20px',
     left: '20px'
@@ -21,20 +138,37 @@ function ClientPlugin(eventBus, canvas) {
   });
 }
 
-ClientPlugin.$inject = [ 'eventBus', 'canvas' ];
+ExamplePluginService.$inject = [ 'eventBus', 'canvas' ];
 
-module.exports = {
-  __init__: [ 'clientPlugin' ],
-  clientPlugin: [ 'type', ClientPlugin ]
-};
 
-},{}],2:[function(require,module,exports){
-var registerBpmnJSPlugin = require('camunda-modeler-plugin-helpers').registerBpmnJSPlugin;
-var plugin = require('./ClientPlugin');
+/**
+ * The service and it's dependencies, exposed as a bpmn-js module.
+ *
+ * --------
+ *
+ * WARNING: This is an example plug-in.
+ *
+ * Make sure you rename the plugin and the name it is exposed (PLEASE_CHANGE_ME)
+ * to something unique.
+ *
+ * --------
+ *
+ */
+/* harmony default export */ __webpack_exports__["default"] = ({
+  __init__: [ 'PLEASE_CHANGE_ME' ],
+  PLEASE_CHANGE_ME: [ 'type', ExamplePluginService ]
+});
 
-registerBpmnJSPlugin(plugin);
 
-},{"./ClientPlugin":1,"camunda-modeler-plugin-helpers":3}],3:[function(require,module,exports){
+/***/ }),
+
+/***/ "./node_modules/camunda-modeler-plugin-helpers/index.js":
+/*!**************************************************************!*\
+  !*** ./node_modules/camunda-modeler-plugin-helpers/index.js ***!
+  \**************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
 /**
  * Validate and register a client plugin.
  *
@@ -77,4 +211,26 @@ function registerBpmnJSPlugin(plugin) {
 
 module.exports.registerBpmnJSPlugin = registerBpmnJSPlugin;
 
-},{}]},{},[2]);
+/**
+ * Validate and register a bpmn-moddle extension plugin.
+ *
+ * Example use:
+ *
+ *    var registerBpmnJSModdleExtension = require('./camundaModelerPluginHelpers').registerBpmnJSModdleExtension;
+ *    var module = require('./index');
+ *
+ *    registerBpmnJSModdleExtension(module);
+ *
+ * @param {Object} plugin
+ */
+function registerBpmnJSModdleExtension(plugin) {
+  registerClientPlugin(plugin, 'bpmn.modeler.moddleExtension');
+}
+
+module.exports.registerBpmnJSModdleExtension = registerBpmnJSModdleExtension;
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=client-bundle.js.map
